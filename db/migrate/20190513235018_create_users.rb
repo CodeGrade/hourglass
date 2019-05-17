93f625d54c68f242f800a61cbb9b72423f8aeedf
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2019-05-17 17:17:23 -0400

    remove irreversible migration

--- db/migrate/20190513235018_create_users.rb
+++ db/migrate/20190513235018_create_users.rb
@@ -2,8 +2,10 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username, null: false
-      t.index ["username"], name: "index_users_on_username", unique: true
+      t.string :encrypted_password, null: false, default: ""
+      t.datetime :remember_created_at
 
+      t.index ["username"], name: "index_users_on_username", unique: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_153211) do
+ActiveRecord::Schema.define(version: 2019_05_13_235656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,12 +53,10 @@ ActiveRecord::Schema.define(version: 2019_05_16_153211) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 

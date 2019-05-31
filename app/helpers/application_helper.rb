65eb5efa53f9d0458f87e0c0842895284e389ce0
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2019-05-30 23:35:55 -0400

    add 'binary?' and 'code_textarea' to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     }
   end
 
-  def mime_type(full_path)
+  def self.mime_type(full_path)
     case File.extname(full_path).downcase
     when ".java"
       "text/x-java"
@@ -77,6 +77,48 @@ module ApplicationHelper
     end
   end
 
+  def self.binary?(mimetype)
+    # NOTE: The mimetypes here must match the ones produced by mime_type above
+    # NOTE: text/unknown is treated as binary, so that (a) browsers won't try to execute it,
+    # but (b) it won't be forced to UTF-8 in Submission#get_submission_files
+    case mimetype
+    when "text/x-java",
+        "text/javascript",
+        "pyret",
+        "scheme",
+        "mllike",
+        "text/markdown",
+        "text/x-ebnf",
+        "text/x-csrc",
+        "text/x-c++src",
+        "text/x-csharp",
+        "text/x-python",
+        "application/xml",
+        "text/x-yaml",
+        "text/html",
+        "text/css",
+        "text/plain",
+        "text/x-makefile"
+      false
+    when "application/java-vm",
+        "image/gif",
+        "image/jpeg",
+        "image/png",
+        "image/tiff",
+        "image/webp",
+        "jar",
+        "zip",
+        "application/x-7z-compressed",
+        "application/pdf",
+        "application/rtf",
+        "audio/mpeg",
+        "text/unknown"
+      true
+    else
+      true
+    end
+  end
+
   def self.capture3(*cmd, stdin_data: '', binmode: false, timeout: nil, signal: :TERM, **opts)
     Open3.popen3(*cmd, opts) do |i, o, e, t|
       if binmode
@@ -101,5 +143,20 @@ module ApplicationHelper
       end
       [out_reader.value, err_reader.value, t.value, timed_out]
     end
+  end
+
+  def code_textarea(str, options = {})
+    # Within textareas, the initial newline is ignored
+    # (https://w3c.github.io/html/syntax.html#restrictions-on-content-models)
+    # so to handle files that start with whitespace, we have to be a bit careful.
+
+    # The content_tag function will implicitly add a newline always, and using
+    # a helper renderer function (as opposed to writing a <textarea> tag directly),
+    # avoids any confusion of how to use it
+
+    # The string will be rendered to html (to escape any special characters),
+    # unless it's already been explicitly marked as html_safe.
+    str = render(html: str) unless str.html_safe?
+    content_tag(:textarea, str, options)
   end
 end

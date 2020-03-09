6a320bf3f0c157245629cebb0888c4b07f41dd88
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-03-09 18:35:23 -0400

    react:install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,3 +29,7 @@ global.displayCode = displayCode; // TODO remove
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);

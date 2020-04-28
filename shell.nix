b81403031c94d566c46015760656b92d5c34dfde
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-04-28 11:53:14 -0400

    fix local database setup

--- shell.nix
+++ shell.nix
@@ -23,7 +23,7 @@ let
     export PGHOST=$PWD/postgres
     export LOG_PATH=$PWD/postgres/LOG
     export PGDATABASE=postgres
-    export DATABASE_URL="postgresql:///postgres?host=$PGHOST"
+    export DATABASE_URL="postgresql:///hourglass?host=$PGHOST"
     export DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL=true
     if [ ! -d $PGHOST ]; then
       mkdir -p $PGHOST

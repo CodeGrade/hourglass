1175d4e5f2b9f255de93d60c62da2918d8a3c38c
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-04-28 11:35:53 -0400

    clean up fixtures

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MainControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get main_home_url
-    assert_response :success
+    get root_url
+    assert_redirected_to new_user_session_path
   end
 end

d1706da79c75d62c4db42f68abebea429deee4bf
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-04-28 11:56:20 -0400

    remove old codemirror globals

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,6 @@ import './bootstrap.js';
 import './bootstrap.scss';
 import './codemirror.js';
 
-import { activateCode, displayCode } from '../exam.js';
-
 require('@rails/ujs').start();
 require('@rails/activestorage').start();
 require('channels');
@@ -20,9 +18,6 @@ require('channels');
 require('codemirror');
 require('jquery');
 require('jquery-ui');
-
-global.activateCode = activateCode; // TODO remove
-global.displayCode = displayCode; // TODO remove
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

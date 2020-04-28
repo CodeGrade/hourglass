ac899c836573c123a523938abf85cf8ce011de9a
Ben Lerner <blerner@ccs.neu.edu>
2020-04-28 09:42:28 -0400

    prettier UI for snapshot status

--- app/javascript/containers/SnapshotInfo.tsx
+++ app/javascript/containers/SnapshotInfo.tsx
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { ExamState } from '../types';
 import { loadSnapshot, saveSnapshot } from '../actions';
+import { MdCloudDone, MdError } from 'react-icons/md';
 
 const TIMEOUT = 10000;
 
 
 function SnapshotInfo(props) {
-  const { load, save, isLoading, success, message  } = props;
+  const { load, save, isLoading, success, message, size = "1.5em" } = props;
   useEffect(() => {
     load();
   }, [load]);
@@ -17,19 +18,23 @@ function SnapshotInfo(props) {
       clearInterval(timer);
     };
   }, [save]);
-  return (
-    <div>
-      <div>
-        {isLoading &&
-         <p>LOADING</p> ||
-         <div>
-           <p>SUCCESS: {success ? "true" : "false"}</p>
-           <p>MESSAGE: {message}</p>
-         </div>
-        }
+  if (isLoading) {
+    return (<button className="btn btn-info" type="button" disabled>
+      <div className={"spinner-border align-middle"} style={{width: size, height: size}} role={"status"}>
+        <span className={"sr-only"}>Loading...</span>
       </div>
-    </div>
-  );
+    </button>);
+  } else if (success) {
+    return (<button className="btn btn-success" type="button" disabled>
+      <MdCloudDone size={size} role="status" />
+      <span className={"sr-only"}>Answers saved to server</span>
+    </button>);
+  } else {
+    return (<button className="btn btn-danger" type="button" disabled role={"status"}>
+      <MdError title={message} size={size} />
+      <span className={"sr-only"}>Error saving answers to server</span>
+    </button>);
+  }
 }
 
 

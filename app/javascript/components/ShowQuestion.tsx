bacd641f5448a352ac390dc9bdae21cfbd8f008a
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-04 12:53:00 -0400

    remove 'jump to part' buttons

--- app/javascript/components/ShowQuestion.tsx
+++ app/javascript/components/ShowQuestion.tsx
@@ -18,36 +18,14 @@ const ShowQuestion: React.FC<ShowQuestionProps> = (props) => {
     name,
     reference,
     description,
-    separateSubparts,
+    separateSubparts, // TODO
     parts,
   } = question;
-  let jumpToPart = null;
-  if (separateSubparts) {
-    jumpToPart = (
-      <div className="mt-4 question-nav d-print-none">
-        <b>Jump to part:</b>
-        <nav aria-label="part navigation" className="d-inline-block">
-          <ul className="pagination">
-            {parts.map((p, i) => (
-              <li key={i} className="page-item part-page-item" id={`nav-q${qnum + 1}p${i + 1}`}>
-                <a href="#" className="page-link question-link">
-                  {qnum + 1}
-                  -
-                  {i + 1}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    );
-  }
   return (
     <div id={`question-${qnum}`} className={`question no-gutters ${separateSubparts ? 'paginated' : ''}`}>
       <h1>{`Question ${qnum + 1}: ${name}`}</h1>
       <HTML value={description} />
       {reference && <FileViewer references={reference} />}
-      {jumpToPart}
       {parts.map((p, i) => (
         <Part
           part={p}

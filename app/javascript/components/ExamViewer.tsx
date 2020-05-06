27f96db2325837823e9a017e800fa5037061a50d
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-06 14:51:04 -0400

    examviewer: show instructions

--- app/javascript/components/ExamViewer.tsx
+++ app/javascript/components/ExamViewer.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { ExamInfo, Exam, AnswersState } from '@hourglass/types';
+import HTML from '@hourglass/components/HTML';
 
-const ExamViewer: React.FC<{}> = () => (
-  <Row>
-    <Col>
+interface ExamViewerProps {
+  info: ExamInfo;
+  contents: {
+    exam: Exam;
+    answers: AnswersState;
+  };
+}
+
+const ExamViewer: React.FC<ExamViewerProps> = (props) => {
+  const {
+    contents,
+  } = props;
+  const {
+    exam,
+  } = contents;
+  const {
+    instructions,
+  } = exam;
+  return (
+    <div>
+      <HTML value={instructions} />
       <p>TODO: ExamViewer</p>
-    </Col>
-  </Row>
-);
+    </div>
+  );
+};
 
 export default ExamViewer;

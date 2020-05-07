5b8118932c15c3d460ec925c9f44e59c140a6a9e
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-07 16:06:41 -0400

    add bootstrap.js back

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import './bootstrap';
 import './bootstrap.scss';
 import './codemirror';
 

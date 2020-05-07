2e34aa216fc21165e3ff59c1ddcdecd99d8abf10
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-07 10:48:11 -0400

    remove unused import

--- app/javascript/components/questions/DisplayCodeTag.tsx
+++ app/javascript/components/questions/DisplayCodeTag.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FileRef, CodeTagInfo, CodeTagState } from '@hourglass/types';
 import {
   Row, Col, Modal, Button,

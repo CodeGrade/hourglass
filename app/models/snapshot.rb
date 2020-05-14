8e1174baa6abf7ab8cfbac9ead9bf3d15a2c97cd
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-14 17:22:09 -0400

    add snapshot model

---
+++ app/models/snapshot.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Mapping from timestamp to student's current answers.
+class Snapshot < ApplicationRecord
+  belongs_to :registration
+
+  delegate :user, to: :registration
+  delegate :exam, to: :registration
+end

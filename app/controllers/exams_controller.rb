c611bccb8d08957b6c2d46276ff6215da70b5acb
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-15 15:55:38 -0400

    fix upload var name

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -134,7 +134,7 @@ class ExamsController < ApplicationController
   def create
     exam_params = params.require(:exam).permit(:name, :file, :enabled)
     upload = Upload.new(exam_params[:file], current_user)
-    Audit.log("Uploaded file #{@upload.original_filename} for #{current_user.username} (#{current_user.id})")
+    Audit.log("Uploaded file #{upload.original_filename} for #{current_user.username} (#{current_user.id})")
     @exam = Exam.new(
       name: exam_params[:name],
       enabled: exam_params[:enabled],

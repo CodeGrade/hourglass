fa97f9bd052e7a4cceca024ed20583c6c7aa4709
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-19 15:11:05 -0400

    sign-in flash

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       @user.update_bottlenose_credentials(request.env["omniauth.auth"])
       sign_in_and_redirect @user, event: :authentication
-      # set_flash_message(:notice, :success, kind: 'Bottlenose') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'Bottlenose') if is_navigational_format?
     else
       session["devise.bottlenose_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url

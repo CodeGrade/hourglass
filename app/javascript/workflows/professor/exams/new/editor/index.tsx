f39ba2b869e31fc404dfb2993062d98f4ce8df64
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-21 14:57:03 -0400

    start exams/new page

---
+++ app/javascript/workflows/professor/exams/new/editor/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Editor: React.FC<{}> = () => <p>TODO</p>;
+export default Editor;
---
+++ app/javascript/workflows/professor/exams/new/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Editor from '@professor/exams/new/editor';
+
+const NewExam: React.FC<{}> = () => (
+  <div>
+    <h1>TODO</h1>
+    <Editor />
+  </div>
+);
+export default NewExam;

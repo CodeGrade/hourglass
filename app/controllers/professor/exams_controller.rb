eeba9e038007a47c80d38defd8f3bec0d6b6ebd3
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-21 13:49:50 -0400

    make sure professor is registered for the course when they try to create an exam

--- app/controllers/professor/exams_controller.rb
+++ app/controllers/professor/exams_controller.rb
@@ -14,19 +14,25 @@ class Professor::ExamsController < ProfessorController
         [c['name'], c['id']]
       end
     end.flatten(1)
-    pp @courses
   end
 
   def create
-    # TODO make sure prof is registered for the course in BN before creating
     exam_params = params.require(:exam).permit(:name, :file, :course_id, :enabled)
+    course_id = exam_params[:course_id]
+    courses = bottlenose_token.get('/api/courses').parsed
+    ids = courses.map { |sem| sem['courses'].map { |c| c['id'].to_s } }.flatten(1)
+    unless ids.member? course_id
+      redirect_back fallback_location: root_path, alert: 'You are not registered for that course.'
+      return
+    end
+
     file = exam_params[:file]
     upload = Upload.new(file)
     Audit.log("Uploaded file #{file.original_filename} for #{current_user.username} (#{current_user.id})")
     @exam = Exam.new(
       name: exam_params[:name],
       enabled: exam_params[:enabled],
-      course_id: exam_params[:course_id],
+      course_id: course_id,
       info: upload.info,
       files: upload.files
     )
@@ -41,7 +47,7 @@ class Professor::ExamsController < ProfessorController
       role: current_user.role.to_s,
       room: room
     )
-    redirect_to ['professor', @exam]
+    redirect_to professor_exam_path(@exam), notice: 'Exam created.'
   end
   
   def show; end

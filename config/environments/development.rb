541542338a1f824c32d2266ff34938e6028afc51
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-28 11:14:10 -0400

    remove more sprockets config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,22 +42,10 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
-  config.assets.digest = false
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # inline source maps for sass
-  config.sass.inline_source_maps = true
 end

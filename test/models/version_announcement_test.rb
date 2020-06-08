9e03929d850470d836e159199f9fe9a6235972ce
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-08 11:25:12 -0400

    fix version announcement tests

--- test/models/version_announcement_test.rb
+++ test/models/version_announcement_test.rb
@@ -2,18 +2,18 @@
 
 require 'test_helper'
 
-class ExamExamAnnouncementTest < ActiveSupport::TestCase
+class VersionAnnouncementTest < ActiveSupport::TestCase
   test 'should save valid announcement' do
-    announcement = ExamAnnouncement.new(
-      exam: exams(:cs2500midterm),
+    announcement = VersionAnnouncement.new(
+      exam_version: exam_versions(:cs2500midterm_1),
       body: 'This is a valid announcement'
     )
     assert announcement.save
   end
 
   test 'should not save announcement without body' do
-    announcement = ExamAnnouncement.new(
-      exam: exams(:cs2500midterm),
+    announcement = VersionAnnouncement.new(
+      exam_version: exam_versions(:cs2500midterm_1),
       body: ''
     )
     assert_not announcement.save

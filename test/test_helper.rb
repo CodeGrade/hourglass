90e5acb43e0f5fe589caa9f360b7bbd6837d0c67
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-09 20:27:40 -0400

    use minitest reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all

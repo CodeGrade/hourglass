2f57cf7c5668c53a5caef39e9f5d2e988ed2b6d1
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-09 20:29:16 -0400

    use spec reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 require 'minitest/reporters'
-Minitest::Reporters.use!
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.

a21fe03cf9bc416f20367be687b9f68bf822a89d
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-10 10:54:56 -0400

    update exam version tests

--- test/models/exam_version_test.rb
+++ test/models/exam_version_test.rb
@@ -3,19 +3,12 @@
 require 'test_helper'
 
 class ExamVersionTest < ActiveSupport::TestCase
-  test 'cs2500 midterm v1 is valid' do
-    assert exam_versions(:cs2500midterm_1).valid?
+  test 'factory builds valid exams' do
+    assert build(:exam_version).valid?
   end
 
-  test 'cs2500 midterm v2 is valid' do
-    assert exam_versions(:cs2500midterm_2).valid?
-  end
-
-  test 'cs2500 midterm v2 info validates' do
-    assert JSON::Validator.validate!(ExamVersion::EXAM_SAVE_SCHEMA, exam_versions(:cs2500midterm_2).info)
-  end
-
-  test 'cs3500 final v1 is valid' do
-    assert exam_versions(:cs3500final_1).valid?
+  test 'exam info validates' do
+    ev = build(:exam_version)
+    assert JSON::Validator.validate!(ExamVersion::EXAM_SAVE_SCHEMA, ev.info)
   end
 end

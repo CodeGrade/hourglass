f6c88a0386cfa7a29f266cba455b2a50f474826c
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-11 15:28:35 -0400

    include devise helpers in integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,13 @@ require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 
+class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include FactoryBot::Syntax::Methods
+  include Devise::Test::IntegrationHelpers
 end

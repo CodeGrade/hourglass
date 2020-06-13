a9a308b8990753d9370476eced03e9ebab8bba7a
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-13 11:42:30 -0400

    move fixture_zip method

--- test/factories/exam_version.rb
+++ test/factories/exam_version.rb
@@ -1,18 +1,5 @@
 # frozen_string_literal: true
 
-def fixture_zip(name)
-  Dir.mktmpdir do |path|
-    dir = Pathname.new path
-    zip = dir.join("#{name}.zip")
-    exam_path = Rails.root.join('test', 'fixtures', 'files', name, '**')
-    ArchiveUtils.create_zip zip, Dir.glob(exam_path)
-    real_upload = ActionDispatch::Http::UploadedFile.new(
-      tempfile: File.new(zip)
-    )
-    Upload.new(real_upload)
-  end
-end
-
 FactoryBot.define do
   factory :exam_version do
     cs2500_v1

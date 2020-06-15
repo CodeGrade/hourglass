f35240d33f8f902eac2f5e2536087738552dbc5c
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-15 18:03:09 -0400

    add parts

--- app/javascript/components/workflows/professor/exams/new/editor/components/ShowParts.tsx
+++ app/javascript/components/workflows/professor/exams/new/editor/components/ShowParts.tsx
@@ -1,65 +1,54 @@
 import React from 'react';
+import {
+  Row,
+  Col,
+  Button,
+} from 'react-bootstrap';
 import { WrappedFieldArrayProps } from 'redux-form';
 import { PartInfo } from '@hourglass/workflows/student/exams/show/types';
+import Part from '@professor/exams/new/editor/containers/Part';
 
 const ShowParts: React.FC<WrappedFieldArrayProps<PartInfo>> = (props) => {
+  const {
+    fields,
+  } = props;
   return (
-    <p>TODO: SHOW PARTS</p>
+    <>
+      <Row>
+        <Col>
+          {fields.map((member, index) => (
+            <p key={index}>TODO: {member}</p>
+          ))}
+        </Col>
+      </Row>
+      <Row className="text-center">
+        <Col>
+          <Button
+            variant="success"
+            onClick={(): void => {
+              const p: PartInfo = {
+                reference: [],
+                name: {
+                  type: 'HTML',
+                  value: '',
+                },
+                description: {
+                  type: 'HTML',
+                  value: '',
+                },
+                points: 0,
+                body: [],
+              };
+              fields.push(p);
+            }}
+          >
+            Add part
+          </Button>
+        </Col>
+      </Row>
+    </>
+
   );
 };
-
-// import Part from '@professor/exams/new/editor/containers/Part';
-// import {
-//   Row,
-//   Col,
-//   Button,
-// } from 'react-bootstrap';
-// import { PartInfo } from '@student/exams/show/types';
-//
-// export interface PartsProps {
-//   qnum: number;
-//   numParts: number;
-//   addPart: (pnum: number, part: PartInfo) => void;
-// }
-//
-// const ShowParts: React.FC<PartsProps> = (props) => {
-//   const {
-//     qnum,
-//     numParts,
-//     addPart,
-//   } = props;
-//   return (
-//     <>
-//       {Array.from(Array(numParts).keys()).map((_, pnum) => (
-//       // eslint-disable-next-line react/no-array-index-key
-//         <Part key={pnum} qnum={qnum} pnum={pnum} numParts={numParts} />
-//       ))}
-//       <Row className="text-center">
-//         <Col>
-//           <Button
-//             variant="success"
-//             onClick={(): void => {
-//               addPart(numParts, {
-//                 name: {
-//                   type: 'HTML',
-//                   value: '',
-//                 },
-//                 description: {
-//                   type: 'HTML',
-//                   value: '',
-//                 },
-//                 points: 0,
-//                 body: [],
-//               });
-//             }}
-//           >
-//             Add part
-//           </Button>
-//         </Col>
-//       </Row>
-//
-//     </>
-//   );
-// };
 
 export default ShowParts;

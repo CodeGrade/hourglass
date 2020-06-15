2471c63f098fd18a7a43c14d3d70adbc5541b7d0
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-15 16:26:14 -0400

    add question button

--- app/javascript/components/workflows/professor/exams/new/editor/components/ShowQuestions.tsx
+++ app/javascript/components/workflows/professor/exams/new/editor/components/ShowQuestions.tsx
@@ -1,67 +1,54 @@
 import React from 'react';
 import { WrappedFieldArrayProps } from 'redux-form';
-// import {
-//   Row,
-//   Col,
-//   Button,
-// } from 'react-bootstrap';
-// import { QuestionInfo } from '@student/exams/show/types';
+import {
+  Row,
+  Col,
+  Button,
+} from 'react-bootstrap';
+import { QuestionInfo } from '@student/exams/show/types';
 // import Question from '@professor/exams/new/editor/containers/Question';
 
 const ShowQuestions: React.FC<WrappedFieldArrayProps> = (props) => {
-  console.log(props);
+  const {
+    fields,
+  } = props;
   return (
-    <p>Questions</p>
+    <>
+      <Row className="py-3">
+        <Col>
+          TODO
+          {/* {questions.map((_q, i) => ( */}
+          {/*   // eslint-disable-next-line react/no-array-index-key */}
+          {/*   <Question key={i} qnum={i} numQuestions={questions.length} /> */}
+          {/* ))} */}
+        </Col>
+      </Row>
+      <Row className="text-center">
+        <Col>
+          <Button
+            variant="primary"
+            onClick={(): void => {
+              const q: QuestionInfo = {
+                name: {
+                  type: 'HTML',
+                  value: '',
+                },
+                description: {
+                  type: 'HTML',
+                  value: '',
+                },
+                parts: [],
+                separateSubparts: false,
+              };
+              fields.push(q);
+            }}
+          >
+            Add question
+          </Button>
+        </Col>
+      </Row>
+    </>
   );
 };
-
-// interface QuestionsProps {
-//   numQuestions: number;
-//   questions: QuestionInfo[];
-//   addQuestion: (qnum: number, question: QuestionInfo) => void;
-// }
-//
-// const ShowQuestions: React.FC<QuestionsProps> = (props) => {
-//   const {
-//     numQuestions,
-//     questions,
-//     addQuestion,
-//   } = props;
-//   return (
-//     <>
-//       <Row className="py-3">
-//         <Col>
-//           {questions.map((_q, i) => (
-//             // eslint-disable-next-line react/no-array-index-key
-//             <Question key={i} qnum={i} numQuestions={questions.length} />
-//           ))}
-//         </Col>
-//       </Row>
-//       <Row className="text-center">
-//         <Col>
-//           <Button
-//             variant="primary"
-//             onClick={(): void => {
-//               addQuestion(numQuestions, {
-//                 name: {
-//                   type: 'HTML',
-//                   value: '',
-//                 },
-//                 description: {
-//                   type: 'HTML',
-//                   value: '',
-//                 },
-//                 parts: [],
-//                 separateSubparts: false,
-//               });
-//             }}
-//           >
-//             Add question
-//           </Button>
-//         </Col>
-//       </Row>
-//     </>
-//   );
-// };
 
 export default ShowQuestions;

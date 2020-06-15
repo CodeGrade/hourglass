e283bb4fa7d81b904432cba1fc46b61beedbea09
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-15 15:30:04 -0400

    reference

--- app/javascript/components/workflows/professor/exams/new/editor/components/ExamEditor.tsx
+++ app/javascript/components/workflows/professor/exams/new/editor/components/ExamEditor.tsx
@@ -31,52 +31,17 @@ const Editor: React.FC<ExamEditorProps> = (props) => {
     reference = [],
     files = [],
   } = exam;
-  const [open, setOpen] = useState(false);
-  const noFiles = reference.length === 0;
   const fmap = createMap(files);
-  const filteredFiles = getFilesForRefs(fmap, reference);
   return (
-    <ExamFilesContext.Provider
-      value={{
-        references: reference,
-      }}
-    >
-      <Container fluid className="flex-fill">
-        <FileUploader />
+    <Container fluid className="flex-fill">
+      <FileUploader />
 
-        <Alert variant="info">
-          <h3>Exam-wide information</h3>
-          <Instructions />
-          <p>Choose files to be shown for the entire exam</p>
-          <InputGroup>
-            <div className="flex-grow-1">
-              <FilePickerExam />
-            </div>
-            <InputGroup.Append>
-              <Button
-                variant="info"
-                disabled={noFiles}
-                onClick={(): void => setOpen((o) => !o)}
-              >
-                Preview files
-                {open && !noFiles ? <FaChevronUp className="ml-2" /> : <FaChevronDown className="ml-2" />}
-              </Button>
-            </InputGroup.Append>
-          </InputGroup>
-          <Collapse in={open && !noFiles}>
-            <div className="border">
-              <VeryControlledFileViewer files={filteredFiles} />
-            </div>
-          </Collapse>
+      <ShowQuestions questions={questions} />
 
-        </Alert>
-        <ShowQuestions questions={questions} />
-
-        <div className="my-2 float-right">
-          <Submit />
-        </div>
-      </Container>
-    </ExamFilesContext.Provider>
+      <div className="my-2 float-right">
+        <Submit />
+      </div>
+    </Container>
   );
 };
 

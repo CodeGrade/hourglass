ba97843fcdb8114934fee41264602c74f4f58a40
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-17 11:12:09 -0400

    edit supplied code

--- app/javascript/components/workflows/professor/exams/new/editor/components/questions/Code.tsx
+++ app/javascript/components/workflows/professor/exams/new/editor/components/questions/Code.tsx
@@ -27,12 +27,14 @@ import {
 // const fileMarks = f?.marks ?? [];
 
 
-const EditLang: React.FC<WrappedFieldProps> = (props) => {
-  const { input } = props;
+const EditLang: React.FC<{
+  value: CodeInfo['lang'];
+  onChange: (newLang: CodeInfo['lang']) => void;
+}> = (props) => {
   const {
     value,
     onChange,
-  } = input;
+  } = props;
   return (
     <Form.Control
       as="select"
@@ -40,7 +42,7 @@ const EditLang: React.FC<WrappedFieldProps> = (props) => {
       size="sm"
       className="col-sm-2"
       value={value}
-      onChange={onChange}
+      onChange={(e): void => onChange(e.target.value)}
     >
       <option value="scheme">Racket</option>
       <option value="text/x-java">Java</option>
@@ -48,26 +50,18 @@ const EditLang: React.FC<WrappedFieldProps> = (props) => {
   );
 };
 
-const EditCodeAnswer: React.FC<WrappedFieldsProps & {
-  qnum: number;
-  pnum: number;
-  bnum: number;
+const EditCodeAnswerValues: React.FC<{
+  lang: CodeInfo['lang'];
+  onChangeLang: (newLang: CodeInfo['lang']) => void;
+  value: CodeState;
+  onChangeVal: (newVal: CodeState) => void;
 }> = (props) => {
   const {
     lang,
-    answer,
-    qnum,
-    pnum,
-    bnum,
-  } = props;
-  const { input } = answer;
-  const {
+    onChangeLang,
     value,
-    onChange,
-  }: {
-    value: CodeState,
-    onChange: (newVal: CodeState) => void;
-  } = input;
+    onChangeVal,
+  } = props;
   const answerText = value?.text ?? '';
   const answerMarks = value?.marks ?? [];
   const [lockState, setLockState] = useState<LockStateInfo>({
@@ -85,108 +79,116 @@ const EditCodeAnswer: React.FC<WrappedFieldsProps & {
     title = 'No region selected to lock';
   }
   return (
-    <>
-      <Form.Group as={Row} controlId={`${qnum}-${pnum}-${bnum}-answer`}>
-        <Form.Label column sm={2}>Answer</Form.Label>
-        <Col sm={10}>
-          <div className="quill bg-white">
-            <div className="ql-toolbar ql-snow">
-              <EditLang input={lang.input} meta={lang.meta} />
-              <Button
-                className="float-none"
-                variant="outline-secondary"
-                size="sm"
-                disabled={!lockState.enabled}
-                active={lockState.active}
-                title={title}
-                onClick={(): void => {
-                  const newMarks = [...answerMarks];
-                  const { curRange, finalPos } = lockState;
-                  if (lockState.active) {
-                    const markId = answerMarks.findIndex((m) => (
-                      m.from.ch === curRange.from.ch
-                      && m.from.line === curRange.from.line
-                      && m.to.ch === curRange.to.ch
-                      && m.to.line === curRange.to.line
-                    ));
-                    newMarks.splice(markId, 1);
-                  } else {
-                    const newMark: MarkDescription = {
-                      from: {
-                        // don't include sticky
-                        ch: curRange.from.ch,
-                        line: curRange.from.line,
-                      },
-                      to: {
-                        // don't include sticky
-                        ch: curRange.to.ch,
-                        line: curRange.to.line,
-                      },
-                      options: {
-                        inclusiveLeft: curRange.from.line === 0 && curRange.from.ch === 0,
-                        inclusiveRight:
-                          curRange.to.line === finalPos.line
-                        && curRange.to.ch === finalPos.ch,
-                      },
-                    };
-                    newMarks.push(newMark);
-                  }
-                  onChange({ text: answerText, marks: newMarks });
-                }}
-              >
-                <FaLock />
-              </Button>
-              <Button
-                className="float-none"
-                variant="outline-secondary"
-                size="sm"
-                title="Clear all locked regions"
-                onClick={(): void => {
-                  onChange({ text: answerText, marks: [] });
-                }}
-              >
-                <FaBan />
-              </Button>
-            </div>
-            <Editor
-              value={answerText}
-              markDescriptions={answerMarks}
-              valueUpdate={[answerMarks]}
-              language={lang.input.value}
-              onSelection={(editor, data): void => {
-                const { ranges, origin } = data;
-                if (origin === undefined) return;
-                if (ranges.length > 0) {
-                  const curRange = ranges[0];
-                  const selectedMarks = editor.findMarks(curRange.from(), curRange.to());
-                  const lastLine = editor.lastLine();
-                  const lastLineLen = editor.getLine(lastLine).length;
-                  if (selectedMarks.length === 0) {
-                    setLockState({
-                      enabled: !curRange.empty(),
-                      active: false,
-                      curRange: { from: curRange.from(), to: curRange.to() },
-                      finalPos: { line: lastLine, ch: lastLineLen },
-                    });
-                  } else {
-                    const markRange = selectedMarks[0].find();
-                    setLockState({
-                      active: true,
-                      enabled: true,
-                      curRange: markRange,
-                      finalPos: { line: lastLine, ch: lastLineLen },
-                    });
-                  }
-                }
-              }}
-              onChange={(newText, newMarks): void => {
-                onChange({ text: newText, marks: newMarks });
-              }}
-            />
-          </div>
-        </Col>
-      </Form.Group>
-    </>
+    <div className="quill bg-white">
+      <div className="ql-toolbar ql-snow">
+        <EditLang onChange={onChangeLang} value={lang} />
+        <Button
+          className="float-none"
+          variant="outline-secondary"
+          size="sm"
+          disabled={!lockState.enabled}
+          active={lockState.active}
+          title={title}
+          onClick={(): void => {
+            const newMarks = [...answerMarks];
+            const { curRange, finalPos } = lockState;
+            if (lockState.active) {
+              const markId = answerMarks.findIndex((m) => (
+                m.from.ch === curRange.from.ch
+                && m.from.line === curRange.from.line
+                && m.to.ch === curRange.to.ch
+                && m.to.line === curRange.to.line
+              ));
+              newMarks.splice(markId, 1);
+            } else {
+              const newMark: MarkDescription = {
+                from: curRange.from,
+                to: curRange.to,
+                options: {
+                  inclusiveLeft: curRange.from.line === 0 && curRange.from.ch === 0,
+                  inclusiveRight:
+                    curRange.to.line === finalPos.line
+                  && curRange.to.ch === finalPos.ch,
+                },
+              };
+              newMarks.push(newMark);
+            }
+            onChangeVal({ text: answerText, marks: newMarks });
+          }}
+        >
+          <FaLock />
+        </Button>
+        <Button
+          className="float-none"
+          variant="outline-secondary"
+          size="sm"
+          title="Clear all locked regions"
+          onClick={(): void => {
+            onChangeVal({ text: answerText, marks: [] });
+          }}
+        >
+          <FaBan />
+        </Button>
+      </div>
+      <Editor
+        value={answerText}
+        markDescriptions={answerMarks}
+        valueUpdate={[answerMarks]}
+        language={lang}
+        onSelection={(editor, data): void => {
+          const { ranges, origin } = data;
+          if (origin === undefined) return;
+          if (ranges.length > 0) {
+            const curRange = ranges[0];
+            const selectedMarks = editor.findMarks(curRange.from(), curRange.to());
+            const lastLine = editor.lastLine();
+            const lastLineLen = editor.getLine(lastLine).length;
+            if (selectedMarks.length === 0) {
+              setLockState({
+                enabled: !curRange.empty(),
+                active: false,
+                curRange: { from: curRange.from(), to: curRange.to() },
+                finalPos: { line: lastLine, ch: lastLineLen },
+              });
+            } else {
+              const markRange = selectedMarks[0].find();
+              setLockState({
+                active: true,
+                enabled: true,
+                curRange: markRange,
+                finalPos: { line: lastLine, ch: lastLineLen },
+              });
+            }
+          }
+        }}
+        onChange={(newText, newMarks): void => {
+          onChangeVal({ text: newText, marks: newMarks });
+        }}
+      />
+    </div>
+  );
+};
+
+const EditCodeAnswer: React.FC<WrappedFieldsProps> = (props) => {
+  const {
+    lang,
+    answer,
+  } = props;
+  const { input } = answer;
+  const {
+    value,
+    onChange,
+  }: {
+    value: CodeState,
+    onChange: (newVal: CodeState) => void;
+  } = input;
+  return (
+    <EditCodeAnswerValues
+      lang={lang.input.value}
+      onChangeLang={lang.input.onChange}
+      value={value}
+      onChangeVal={onChange}
+    />
   );
 };
 
@@ -208,8 +210,28 @@ interface LockStateInfo {
   finalPos?: CodeMirror.Position;
 }
 
-const SetInitial: React.FC<WrappedFieldProps> = (props) => {
-  const { input } = props;
+const EditSuppliedCode: React.FC<WrappedFieldsProps> = (props) => {
+  const {
+    lang,
+    initial,
+  } = props;
+  return (
+    <EditCodeAnswerValues
+      lang={lang.input.value}
+      onChangeLang={lang.input.onChange}
+      value={initial.input.value}
+      onChangeVal={initial.input.onChange}
+    />
+  );
+};
+
+const SetInitial: React.FC<WrappedFieldProps & {
+  controlId: string;
+}> = (props) => {
+  const {
+    input,
+    controlId,
+  } = props;
   const {
     value,
     onChange,
@@ -221,38 +243,44 @@ const SetInitial: React.FC<WrappedFieldProps> = (props) => {
   const isText = value && 'text' in value;
   const isNone = !value;
   return (
-    <ButtonGroup>
-      <Button
-        disabled
-        variant={isNone ? 'secondary' : 'outline-secondary'}
-        active={isNone}
-        onClick={(): void => {
-          onChange(null);
-        }}
-      >
-        None
-      </Button>
-      <Button
-        disabled
-        variant={isText ? 'secondary' : 'outline-secondary'}
-        active={isText}
-        onClick={(): void => {
-          onChange({ text: '', marks: [] });
-        }}
-      >
-        Supply code
-      </Button>
-      <Button
-        disabled
-        variant={isFile ? 'secondary' : 'outline-secondary'}
-        active={isFile}
-        onClick={(): void => {
-          onChange(null);
-        }}
-      >
-        Choose a file
-      </Button>
-    </ButtonGroup>
+    <>
+      <ButtonGroup>
+        <Button
+          variant={isNone ? 'secondary' : 'outline-secondary'}
+          active={isNone}
+          onClick={(): void => {
+            onChange(null);
+          }}
+        >
+          None
+        </Button>
+        <Button
+          variant={isText ? 'secondary' : 'outline-secondary'}
+          active={isText}
+          onClick={(): void => {
+            onChange({ text: '', marks: [] });
+          }}
+        >
+          Supply code
+        </Button>
+        <Button
+          variant={isFile ? 'secondary' : 'outline-secondary'}
+          active={isFile}
+          onClick={(): void => {
+            onChange(null);
+          }}
+        >
+          Choose a file
+        </Button>
+      </ButtonGroup>
+      {isText && (
+        <Fields
+          names={['lang', 'initial']}
+          component={EditSuppliedCode}
+          controlId={controlId}
+        />
+      )}
+    </>
   );
 };
 
@@ -262,7 +290,6 @@ const Code: React.FC<CodeProps> = (props) => {
     pnum,
     bnum,
   } = props;
-
   return (
     <>
       <Prompted
@@ -273,10 +300,22 @@ const Code: React.FC<CodeProps> = (props) => {
       <Form.Group as={Row}>
         <Form.Label column sm={2}>Starter</Form.Label>
         <Col sm={10}>
-          <Field name="initial" component={SetInitial} />
+          <Field
+            name="initial"
+            component={SetInitial}
+            controlId={`${qnum}-${pnum}-${bnum}-initial`}
+          />
         </Col>
       </Form.Group>
-      <Fields names={['lang', 'answer']} component={EditCodeAnswer} qnum={qnum} pnum={pnum} bnum={bnum} />
+      <Form.Group as={Row} controlId={`${qnum}-${pnum}-${bnum}-answer`}>
+        <Form.Label column sm={2}>Answer</Form.Label>
+        <Col sm={10}>
+          <Fields
+            names={['lang', 'answer']}
+            component={EditCodeAnswer}
+          />
+        </Col>
+      </Form.Group>
     </>
   );
 };

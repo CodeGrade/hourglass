7f07e36628525dca3b9125cbc41cd42cb31a1f51
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-18 11:05:50 -0400

    blocknav

--- app/javascript/components/workflows/student/exams/show/components/ExamTaker.tsx
+++ app/javascript/components/workflows/student/exams/show/components/ExamTaker.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 import ExamNavbar from '@student/exams/show/containers/navbar';
 import RegularNavbar from '@hourglass/common/navbar';
 import { Row, Col, Container } from 'react-bootstrap';
@@ -6,6 +6,8 @@ import { RailsContext } from '@student/exams/show/context';
 import ExamShowContents from '@student/exams/show/containers/ExamShowContents';
 import PreStart from '@student/exams/show/containers/PreStart';
 import './ExamTaker.scss';
+import { Prompt } from 'react-router-dom';
+import { BlockNav } from '@hourglass/workflows';
 
 interface ExamTakerProps {
   ready: boolean;
@@ -19,9 +21,20 @@ const ExamTaker: React.FC<ExamTakerProps> = (props) => {
     railsCourse,
     railsExam,
   } = useContext(RailsContext);
+  const stay = useCallback(() => {
+    console.log('chose stay');
+  }, []);
+  const leave = useCallback(() => {
+    console.log('chose leave');
+  }, []);
   const body = ready ? (
     <div id="exam-taker" className="d-flex">
       <ExamNavbar />
+      <BlockNav
+        onStay={stay}
+        onLeave={leave}
+        message="Are you sure you want to navigate away? Your exam will be submitted."
+      />
       <Container fluid className="flex-fill transition">
         <Row
           id="exam-body"

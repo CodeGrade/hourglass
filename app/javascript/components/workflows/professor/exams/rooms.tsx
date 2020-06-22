95b2dbbc906236d4f91e59c60117976198313cbb
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-06-22 16:49:14 -0400

    edit rooms mockup

--- app/javascript/components/workflows/professor/exams/rooms.tsx
+++ app/javascript/components/workflows/professor/exams/rooms.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { useResponse as indexRooms } from '@hourglass/common/api/professor/rooms/index';
+import { Room, useResponse as indexRooms } from '@hourglass/common/api/professor/rooms/index';
 import { useParams } from 'react-router-dom';
 import { ExhaustiveSwitchError } from '@hourglass/common/helpers';
+import { Button, Form, FormControl, InputGroup } from 'react-bootstrap';
+import Icon from '@hourglass/workflows/student/exams/show/components/Icon';
+import { FaTrash, FaTrashRestore, FaPlus } from 'react-icons/fa';
 
 const EditExamRooms: React.FC = () => {
   const { examId } = useParams();
@@ -12,15 +15,86 @@ const EditExamRooms: React.FC = () => {
     case 'LOADING':
       return <p>Loading...</p>;
     case 'RESULT':
-      return (
-        <>
-          <p><h1>TODO: Edit/Add rooms</h1></p>
-          {JSON.stringify(response.response)}
-        </>
-      );
+      return <Loaded rooms={response.response.rooms} />;
     default:
       throw new ExhaustiveSwitchError(response);
   }
 };
 
 export default EditExamRooms;
+
+const Loaded: React.FC<{
+  rooms: Room[];
+}> = (props) => {
+  const {
+    rooms,
+  } = props;
+  return (
+    <>
+      <h1>Edit rooms</h1>
+      <ul>
+        {rooms.map((r) => (
+          <li key={r.id}>
+            <Form.Group>
+              <InputGroup>
+                <FormControl
+                  size="lg"
+                  placeholder="Room name"
+                  defaultValue={r.name}
+                />
+                <InputGroup.Append>
+                  <Button
+                    variant="danger"
+                    disabled={r.students.length !== 0}
+                  >
+                    <Icon I={FaTrash} />
+                    <span className="ml-1">
+                      Delete
+                    </span>
+                  </Button>
+                </InputGroup.Append>
+                <InputGroup.Append>
+                  <Button
+                    variant="success"
+                    disabled={r.students.length !== 0}
+                  >
+                    <Icon I={FaTrashRestore} />
+                    <span className="ml-1">
+                      Restore
+                    </span>
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Form.Group>
+          </li>
+        ))}
+      </ul>
+      <Form.Group>
+        <InputGroup>
+          <FormControl
+            size="lg"
+            placeholder="Room name"
+          />
+          <InputGroup.Append>
+            <Button
+              variant="success"
+            >
+              <Icon I={FaPlus} />
+              <span className="ml-1">
+                Add room
+              </span>
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </Form.Group>
+      <Form.Group>
+        <Button
+          size="lg"
+          variant="success"
+        >
+          Submit
+        </Button>
+      </Form.Group>
+    </>
+  );
+};

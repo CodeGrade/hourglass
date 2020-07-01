080d7c188cc642927dee6c25eca4e5d6510011fb
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-01 18:07:25 -0400

    remove puts

--- config/initializers/graphql_schema_updater.rb
+++ config/initializers/graphql_schema_updater.rb
@@ -2,7 +2,6 @@ reloader = ActiveSupport::FileUpdateChecker.new([], {
   Rails.root.join('app/graphql/types').to_s => ['rb'],
   Rails.root.join('app/graphql/mutations').to_s => ['rb']
 }) do
-  puts "test???????????????aaaaaaaaaaaaaaaaaaaa"
   File.open('app/javascript/relay/data/schema.json', 'w') do |f|
     f.write(HourglassSchema.execute(GraphQL::Introspection::INTROSPECTION_QUERY).to_json)
   end

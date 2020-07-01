62613f3fe33025c2e279406d763d7fc5f27fc43b
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-01 18:13:57 -0400

    update schema on rails server restart

--- config/initializers/graphql_schema_updater.rb
+++ config/initializers/graphql_schema_updater.rb
@@ -1,12 +1,20 @@
-reloader = ActiveSupport::FileUpdateChecker.new([], {
-  Rails.root.join('app/graphql/types').to_s => ['rb'],
-  Rails.root.join('app/graphql/mutations').to_s => ['rb']
-}) do
+def write_schema!
   File.open('app/javascript/relay/data/schema.json', 'w') do |f|
     f.write(HourglassSchema.execute(GraphQL::Introspection::INTROSPECTION_QUERY).to_json)
   end
 end
 
+reloader = ActiveSupport::FileUpdateChecker.new([], {
+  Rails.root.join('app/graphql/types').to_s => ['rb'],
+  Rails.root.join('app/graphql/mutations').to_s => ['rb']
+}) do
+  write_schema!
+end
+
 Rails.application.config.to_prepare do
   reloader.execute_if_updated
+end
+
+Rails.application.config.after_initialize do
+  write_schema!
 end

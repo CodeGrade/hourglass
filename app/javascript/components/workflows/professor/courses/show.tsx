b084e2c709a1a9aa294ed4cd9359088f894d3cc5
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-02 11:39:41 -0400

    move prof/courses/show

--- app/javascript/components/workflows/professor/courses/show.tsx
+++ app/javascript/components/workflows/professor/courses/show.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useParams, Link, Route } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
-import { useResponse as showCourse } from '@hourglass/common/api/professor/courses/show';
 import { ExhaustiveSwitchError } from '@hourglass/common/helpers';
 import { useResponse as examsIndex } from '@hourglass/common/api/professor/exams';
 import NewExam from '@professor/exams/new';
 import SyncCourse from '@professor/courses/sync';
 import DocumentTitle from '@hourglass/common/documentTitle';
 import Loading from '@hourglass/common/loading';
+import { QueryRenderer, graphql } from 'react-relay';
+import environment from '@hourglass/relay/environment';
+import { showCourseQuery } from './__generated__/showCourseQuery.graphql';
+
 
 interface CourseExamsProps {
   courseId: number;
@@ -46,59 +49,71 @@ const CourseExams: React.FC<CourseExamsProps> = (props) => {
 
 const ShowCourse: React.FC = () => {
   const { courseId } = useParams();
-  const res = showCourse(courseId);
-  let course;
-  switch (res.type) {
-    case 'ERROR':
-      return <p>Error</p>;
-    case 'LOADING':
-      course = { id: courseId, title: 'Course' };
-      break;
-    case 'RESULT':
-      course = res.response.course;
-      break;
-    default:
-      throw new ExhaustiveSwitchError(res);
-  }
   return (
-    <Loading loading={res.type === 'LOADING'}>
-      <div className="d-flex align-items-center justify-content-between">
-        <h1>
-          {course.title}
-        </h1>
-        <div>
-          <Link to={`/courses/${courseId}/sync`}>
-            <Button
-              variant="danger"
-            >
-              Sync
-            </Button>
-          </Link>
-          <Link to={`/courses/${courseId}/new`} className="ml-1">
-            <Button
-              variant="success"
-            >
-              New Exam
-            </Button>
-          </Link>
-        </div>
-      </div>
-      <Route path="/courses/:courseId" exact>
-        <DocumentTitle title={course.title}>
-          <CourseExams courseId={courseId} />
-        </DocumentTitle>
-      </Route>
-      <Route path="/courses/:courseId/sync" exact>
-        <DocumentTitle title={`Sync - ${course.title}`}>
-          <SyncCourse />
-        </DocumentTitle>
-      </Route>
-      <Route path="/courses/:courseId/new" exact>
-        <DocumentTitle title={`New Exam - ${course.title}`}>
-          <NewExam />
-        </DocumentTitle>
-      </Route>
-    </Loading>
+    <QueryRenderer<showCourseQuery>
+      environment={environment}
+      query={graphql`
+        query showCourseQuery($railsId: Int!) {
+          course(railsId: $railsId) {
+            title
+          }
+        }
+        `}
+      variables={{
+        railsId: Number(courseId),
+      }}
+      render={({ error, props }) => {
+        if (error) {
+          return <p>Error</p>;
+        }
+        let course;
+        if (!props) {
+          course = { id: courseId, title: 'Course' };
+        } else {
+          course = { id: courseId, title: props.course.title };
+        }
+        return (
+          <Loading loading={!props}>
+            <div className="d-flex align-items-center justify-content-between">
+              <h1>
+                {course.title}
+              </h1>
+              <div>
+                <Link to={`/courses/${courseId}/sync`}>
+                  <Button
+                    variant="danger"
+                  >
+                    Sync
+                  </Button>
+                </Link>
+                <Link to={`/courses/${courseId}/new`} className="ml-1">
+                  <Button
+                    variant="success"
+                  >
+                    New Exam
+                  </Button>
+                </Link>
+              </div>
+            </div>
+            <Route path="/courses/:courseId" exact>
+              <DocumentTitle title={course.title}>
+                <CourseExams courseId={courseId} />
+              </DocumentTitle>
+            </Route>
+            <Route path="/courses/:courseId/sync" exact>
+              <DocumentTitle title={`Sync - ${course.title}`}>
+                <SyncCourse />
+              </DocumentTitle>
+            </Route>
+            <Route path="/courses/:courseId/new" exact>
+              <DocumentTitle title={`New Exam - ${course.title}`}>
+                <NewExam />
+              </DocumentTitle>
+            </Route>
+          </Loading>
+        );
+      }}
+    />
   );
 };
 

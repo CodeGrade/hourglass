d9c04bc723dbdd22e5e57b2a8f601f6904517c18
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-02 14:13:24 -0400

    permissions for updateexam

--- app/graphql/mutations/update_exam.rb
+++ app/graphql/mutations/update_exam.rb
@@ -1,7 +1,6 @@
 module Mutations
   class UpdateExam < BaseMutation
-
-    argument :rails_id, Integer, required: true
+    argument :exam_id, ID, required: true, loads: Types::ExamType
     argument :name, String, required: true
     argument :duration, Integer, required: true
     argument :start_time, GraphQL::Types::ISO8601DateTime, required: true
@@ -10,8 +9,18 @@ module Mutations
     field :exam, Types::ExamType, null: true
     field :errors, [String], null: false
 
-    def resolve(rails_id:, name:, duration:, start_time:, end_time:)
-      exam = Exam.find(rails_id)
+    def authorized?(exam:, **args)
+      reg = ProfessorCourseRegistration.find_by(
+        user: context[:current_user],
+        course: exam.course,
+      )
+      if reg.nil?
+        return false, { errors: ['You do not have permission.'] }
+      end
+      true
+    end
+
+    def resolve(exam:, name:, duration:, start_time:, end_time:)
       if exam.update(name: name, duration: duration, start_time: start_time, end_time: end_time)
         {
           exam: exam,

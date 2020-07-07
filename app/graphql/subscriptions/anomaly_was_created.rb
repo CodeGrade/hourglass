de3bc00a551c6da8dcbf4ba74f602a8a8de0fbcf
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-07 16:09:39 -0400

    anomalywascreated errors

--- app/graphql/subscriptions/anomaly_was_created.rb
+++ app/graphql/subscriptions/anomaly_was_created.rb
@@ -1,7 +1,6 @@
 class Subscriptions::AnomalyWasCreated < Subscriptions::BaseSubscription
   argument :exam_rails_id, Integer, required: true
 
-  field :errors, [String], null: false
   field :anomaly, Types::AnomalyType, null: true
   field :anomaliesConnection, Types::AnomalyType.connection_type, null: true
   field :anomalyEdge, Types::AnomalyType.edge_type, null: true
@@ -16,18 +15,14 @@ class Subscriptions::AnomalyWasCreated < Subscriptions::BaseSubscription
       user: context[:current_user],
       course: exam.course,
     )
-    return false, { errors: ['You do not have permission.'] }
-  end
 
-
-  def subscribe(**args)
-    {
-      errors: [],
-    }
+    raise GraphQL::ExecutionError, 'You do not have permission.'
   end
 
   def update(exam_rails_id:)
-    exam = Exam.find_by!(id: exam_rails_id)
+    exam = Exam.find_by(id: exam_rails_id)
+    raise GraphQL::ExecutionError, 'Invalid exam.' unless exam
+
     range_add = GraphQL::Relay::RangeAdd.new(
       parent: exam,
       collection: exam.anomalies,
@@ -36,7 +31,6 @@ class Subscriptions::AnomalyWasCreated < Subscriptions::BaseSubscription
     )
 
     {
-      errors: [],
       anomaly: object,
       anomaliesConnection: range_add.connection,
       anomalyEdge: range_add.edge,

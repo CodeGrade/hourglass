1b85db1f6a8b07e3a9d82da66ad8cd07441daad9
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-08 15:06:58 -0400

    visibility guard

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -3,5 +3,9 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
     field_class Types::BaseField
+
+    module Guards
+      VISIBILITY = ->(obj, _args, ctx) { obj.object.visible_to?(ctx[:current_user]) }
+    end
   end
 end

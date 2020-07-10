0e138cf0ddebe0dd713b33f4096246d85379940c
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-10 17:15:43 -0400

    enable impersonation in controllers and channels

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  impersonates :user
+
   rescue_from DoubleLoginException do |_e|
     redirect_to root_path,
                 alert: 'You are currently logged into another session.'

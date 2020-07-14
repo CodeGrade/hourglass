42d0454a80b73a997b75365a3277d35eea774d40
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-14 17:23:57 -0400

    remove fileref TODO
    opened #181

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -175,7 +175,6 @@ class Upload
                 elsif b.key? 'Code'
                   initial = b['Code']['initial']
                   unless initial.nil?
-                    # TODO: check file ref validity
                     if initial.key? 'file'
                     #   filename = initial['file']
                     #   file = files[filename]

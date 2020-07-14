75310c7599bc703ae583d052c4cea803335d5863
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-14 11:57:17 -0400

    better loading styles

--- app/javascript/components/common/loading.tsx
+++ app/javascript/components/common/loading.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { Spinner } from 'react-bootstrap';
 import LoadingOverlay from 'react-loading-overlay';
 
+const STYLES = {
+  overlay: (base) => ({
+    ...base,
+    borderRadius: '0.25rem',
+  }),
+  content: (base) => ({
+    ...base,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+  }),
+};
+
+const SPINNER = <Spinner animation="border" role="status" />;
+
 const Loading: React.FC<{
   loading: boolean;
   noText?: boolean;
@@ -17,12 +32,8 @@ const Loading: React.FC<{
     <LoadingOverlay
       className={className}
       active={loading}
-      spinner={(
-        <>
-          <Spinner animation="border" role="status" />
-          <br />
-        </>
-      )}
+      styles={STYLES}
+      spinner={SPINNER}
       text={noText ? undefined : 'Loading...'}
     >
       {children}

6ea374c61e239fc82a974689b76e931919fcc6ae
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-15 20:28:35 -0400

    invalid qnum test

--- test/models/grading_check_test.rb
+++ test/models/grading_check_test.rb
@@ -4,7 +4,14 @@ require 'test_helper'
 
 class GradingCheckTest < ActiveSupport::TestCase
   def setup
-    @check = create(:grading_check)
+    @course = create(:course)
+    @section = create(:section, course: @course)
+    @exam = create(:exam, course: @course)
+    @version = create(:exam_version, exam: @exam)
+    @reg = create(:registration, exam_version: @version)
+    @grader = create(:user)
+    @staff_reg = create(:staff_registration, user: @grader, section: @section)
+    @check = create(:grading_check, creator: @grader, registration: @reg)
   end
 
   test 'grading check factory' do
@@ -17,5 +24,11 @@ class GradingCheckTest < ActiveSupport::TestCase
 
   test 'grading check with negative deduction is invalid' do
     assert_not build(:grading_check, deduction: -5).valid?
+  end
+
+  test 'invalid qnum' do
+    bad = build(:grading_check, qnum: @version.questions.length)
+    assert_not bad.valid?
+    assert_match(/valid question/, bad.errors.full_messages.to_sentence)
   end
 end

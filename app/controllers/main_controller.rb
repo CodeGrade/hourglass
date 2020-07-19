e2a3a7acd61c0dfab0415d238bbb42ceaf55f49f
Ben Lerner <blerner@ccs.neu.edu>
2020-07-19 15:09:35 -0400

    remove stray pry

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,6 @@ class MainController < ApplicationController
   def index
     return redirect_to new_user_session_path unless current_user
 
-    binding.pry
     render component: 'workflows', prerender: false, class: 'h-100 d-flex flex-column'
   end
 end

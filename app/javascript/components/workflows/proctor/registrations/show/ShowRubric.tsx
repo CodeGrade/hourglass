2be7b6109f6468fcdf13113694ac60daf690944b
Ben Lerner <blerner@ccs.neu.edu>
2020-07-21 13:01:25 -0400

    UI refinements

--- app/javascript/components/workflows/proctor/registrations/show/ShowRubric.tsx
+++ app/javascript/components/workflows/proctor/registrations/show/ShowRubric.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Rubric,
   Preset,
@@ -15,20 +15,21 @@ import {
   Col,
   ButtonGroup,
   Button,
-  AlertProps,
+  Collapse,
 } from 'react-bootstrap';
 import HTML from '@hourglass/workflows/student/exams/show/components/HTML';
 import Icon from '@hourglass/workflows/student/exams/show/components/Icon';
 import { BsArrowUpRight, BsArrowDownRight } from 'react-icons/bs';
 import { variantForPoints, iconForPoints } from '@grading/index';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
 const ShowPreset: React.FC<{
   preset: Preset;
-  variant: AlertProps['variant'];
+  direction: RubricPresets['direction'];
 }> = (props) => {
   const {
     preset,
-    variant = 'info',
+    direction,
   } = props;
   const {
     label,
@@ -37,7 +38,7 @@ const ShowPreset: React.FC<{
     points,
   } = preset;
   return (
-    <Alert variant={variant} className="p-0">
+    <Alert variant={direction === 'credit' ? 'success' : 'danger'} className="p-0">
       <Button
         disabled
         variant={variantForPoints(points)}
@@ -50,10 +51,14 @@ const ShowPreset: React.FC<{
       {label && <span>{`[${label}]`}</span>}
       <HTML value={graderHint} className="d-inline-block" />
       {studentFeedback && (
-        <p>
-          Sample student message:
-          <HTML value={studentFeedback} className="d-inline-block" />
-        </p>
+        <Row className="p-2">
+          <Col sm="2">
+            <i className="mr-2">Sample student message:</i>
+          </Col>
+          <Col className="border border-info bg-white rounded mr-3">
+            <HTML value={studentFeedback} />
+          </Col>
+        </Row>
       )}
     </Alert>
   );
@@ -72,12 +77,12 @@ const ShowRubricPresets: React.FC<{ choices: RubricPresets }> = (props) => {
       <Row>
         <Col>
           <ButtonGroup className="float-right">
-            <Button variant="outline-secondary" size="sm" disabled>{label}</Button>
-            <Button variant="outline-secondary" size="sm" disabled>
+            <Button variant="outline-secondary" className="bg-white" size="sm" disabled>{label}</Button>
+            <Button variant="outline-secondary" className="bg-white" size="sm" disabled>
               <Icon I={direction === 'credit' ? BsArrowUpRight : BsArrowDownRight} />
             </Button>
             {mercy && (
-              <Button variant="outline-secondary" size="sm" disabled>
+              <Button variant="outline-secondary" className="bg-white" size="sm" disabled>
                 {`Up to ${pluralize(mercy, 'point', 'points')}`}
               </Button>
             )}
@@ -88,7 +93,7 @@ const ShowRubricPresets: React.FC<{ choices: RubricPresets }> = (props) => {
         <Col>
           {presets.map((p, index) => (
             // eslint-disable-next-line react/no-array-index-key
-            <ShowPreset preset={p} key={index} />
+            <ShowPreset direction={direction} preset={p} key={index} />
           ))}
         </Col>
       </Row>
@@ -99,25 +104,38 @@ const ShowRubricPresets: React.FC<{ choices: RubricPresets }> = (props) => {
 const ShowRubricAll: React.FC<{ rubric: RubricAll }> = (props) => {
   const { rubric } = props;
   const { description, choices } = rubric;
+  const [open, setOpen] = useState(false);
   return (
     <div className="rubric">
       <Alert variant="dark">
         <h5>
-          Rubric: Choose something from
-          <i className="mx-1">all</i>
-          entries
+          <span
+            role="button"
+            onClick={() => setOpen((o) => !o)}
+            onKeyPress={() => setOpen((o) => !o)}
+            tabIndex={0}
+          >
+            Rubric: Choose something from
+            <i className="mx-1">all</i>
+            entries
+            <Icon className="ml-2" I={open ? FaChevronUp : FaChevronDown} />
+          </span>
         </h5>
-        <HTML value={description} />
-        {isRubricPresets(choices) ? (
-          <ShowRubricPresets choices={choices} />
-        ) : (
-          <>
-            {choices.map((c, index) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <ShowRubric key={index} rubric={c} />
-            ))}
-          </>
-        )}
+        <Collapse in={open}>
+          <div>
+            <HTML value={description} />
+            {isRubricPresets(choices) ? (
+              <ShowRubricPresets choices={choices} />
+            ) : (
+              <>
+                {choices.map((c, index) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <ShowRubric key={index} rubric={c} />
+                ))}
+              </>
+            )}
+          </div>
+        </Collapse>
       </Alert>
     </div>
   );
@@ -126,27 +144,40 @@ const ShowRubricAll: React.FC<{ rubric: RubricAll }> = (props) => {
 const ShowRubricAny: React.FC<{ rubric: RubricAny }> = (props) => {
   const { rubric } = props;
   const { points, description, choices } = rubric;
+  const [open, setOpen] = useState(false);
   const pointsMsg = `(${pluralize(points, 'point', 'points')})`;
   return (
     <div className="rubric">
       <Alert variant="dark">
         <h5>
-          Rubric: Choose something from
-          <i className="mx-1">any</i>
-          entries
+          <span
+            role="button"
+            onClick={() => setOpen((o) => !o)}
+            onKeyPress={() => setOpen((o) => !o)}
+            tabIndex={0}
+          >
+            Rubric: Choose something from
+            <i className="mx-1">any</i>
+            entries
+            <Icon className="ml-2" I={open ? FaChevronUp : FaChevronDown} />
+          </span>
           <span className="float-right">{pointsMsg}</span>
         </h5>
-        <HTML value={description} />
-        {isRubricPresets(choices) ? (
-          <ShowRubricPresets choices={choices} />
-        ) : (
-          <>
-            {choices.map((c, index) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <ShowRubric key={index} rubric={c} />
-            ))}
-          </>
-        )}
+        <Collapse in={open}>
+          <div>
+            <HTML value={description} />
+            {isRubricPresets(choices) ? (
+              <ShowRubricPresets choices={choices} />
+            ) : (
+              <>
+                {choices.map((c, index) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <ShowRubric key={index} rubric={c} />
+                ))}
+              </>
+            )}
+          </div>
+        </Collapse>
       </Alert>
     </div>
   );
@@ -156,27 +187,40 @@ const ShowRubricAny: React.FC<{ rubric: RubricAny }> = (props) => {
 const ShowRubricOne: React.FC<{ rubric: RubricOne }> = (props) => {
   const { rubric } = props;
   const { points, description, choices } = rubric;
+  const [open, setOpen] = useState(false);
   const pointsMsg = `(${pluralize(points, 'point', 'points')})`;
   return (
     <div className="rubric">
       <Alert variant="dark">
         <h5>
-          Rubric: Choose something from
-          <i className="mx-1">exactly one</i>
-          entry
+          <span
+            role="button"
+            onClick={() => setOpen((o) => !o)}
+            onKeyPress={() => setOpen((o) => !o)}
+            tabIndex={0}
+          >
+            Rubric: Choose something from
+            <i className="mx-1">exactly one</i>
+            entry
+            <Icon className="ml-2" I={open ? FaChevronUp : FaChevronDown} />
+          </span>
           <span className="float-right">{pointsMsg}</span>
         </h5>
-        <HTML value={description} />
-        {isRubricPresets(choices) ? (
-          <ShowRubricPresets choices={choices} />
-        ) : (
-          <>
-            {choices.map((c, index) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <ShowRubric key={index} rubric={c} />
-            ))}
-          </>
-        )}
+        <Collapse in={open}>
+          <div>
+            <HTML value={description} />
+            {isRubricPresets(choices) ? (
+              <ShowRubricPresets choices={choices} />
+            ) : (
+              <>
+                {choices.map((c, index) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <ShowRubric key={index} rubric={c} />
+                ))}
+              </>
+            )}
+          </div>
+        </Collapse>
       </Alert>
     </div>
   );

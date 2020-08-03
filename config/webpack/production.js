079f48447072eecd66940c281047be624fe981fd
Ben Lerner <blerner@ccs.neu.edu>
2020-08-03 16:57:56 -0400

    properly ignore wdyr and graphiql in production mode

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.loaders.prepend('ignore', {
+  test: /graphiql|wdyr/,
+  loader: 'ignore-loader',
+});
+
 module.exports = environment.toWebpackConfig()

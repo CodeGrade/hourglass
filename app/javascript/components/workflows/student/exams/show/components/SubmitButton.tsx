1fa660578b186eaec69360d18ef7ebc76ebd18d0
Ben Lerner <blerner@ccs.neu.edu>
2020-08-06 10:55:38 -0400

    fixes #186

--- app/javascript/components/workflows/student/exams/show/components/SubmitButton.tsx
+++ app/javascript/components/workflows/student/exams/show/components/SubmitButton.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
+  Modal,
 } from 'react-bootstrap';
 
 interface SubmitButtonProps {
@@ -9,13 +10,43 @@ interface SubmitButtonProps {
 
 const SubmitButton: React.FC<SubmitButtonProps> = (props) => {
   const { submit } = props;
+  const [submitting, setSubmitting] = useState(false);
   return (
     <div className="my-2">
+      <Modal
+        centered
+        keyboard
+        show={submitting}
+        onHide={() => { setSubmitting(false); }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure you want to submit?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Once you submit, you will no be able to change your answers,
+          or see your submission until it has been graded.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="success"
+            onClick={() => {
+              setSubmitting(false);
+              submit();
+            }}
+          >
+            Submit Exam
+          </Button>
+          <Button
+            variant="danger"
+            onClick={() => { setSubmitting(false); }}
+          >
+            Continue working
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <Button
         variant="success"
-        onClick={(): void => {
-          submit();
-        }}
+        onClick={() => { setSubmitting(true); }}
       >
         Submit Exam
       </Button>

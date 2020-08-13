a4cf1b561658b02f0917baed6c44a01db3469432
Ben Lerner <blerner@ccs.neu.edu>
2020-08-13 17:49:08 -0400

    more generic factory name

--- test/factories/course.rb
+++ test/factories/course.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :course do
-    title { 'CS 2500' }
+    sequence(:title, 101) {|n| "Computing #{n}" }
     last_sync { '2020-05-22 14:03:53' }
     active { true }
     sequence(:bottlenose_id)

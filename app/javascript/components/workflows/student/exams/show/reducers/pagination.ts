CodeGrade/hourglass

commit bc69dd0e5968ae7e1537b54874f12614ae7e0e4a

hide "Part A" when it's anonymous and not necessary

app/javascript/components/workflows/student/exams/show/reducers/pagination.ts
@@ -80,7 +80,7 @@ export default (state: PaginationState = {
         if (!q.separateSubparts) pageCoords.push(thisQ);
         // Only include parts if there are multiple of them,
         // or if the solo part isn't anonymous
-        if (q.parts.length > 1 || q.parts[0]?.name) {
+        if (q.parts.length > 1 || q.parts[0]?.name?.value) {
           q.parts.forEach((_p, pnum) => {
             const thisP = {
               question: qnum,

app/javascript/components/workflows/proctor/registrations/show/Part.tsx
@@ -8,6 +8,7 @@ import { PartFilesContext, ExamViewerContext } from '@hourglass/common/context';
 import { PartName } from '@student/exams/show/components/Part';
 import ShowRubric from '@proctor/registrations/show/ShowRubric';
 import { CurrentGrading } from '@hourglass/workflows/professor/exams/types';
+import { pluralize } from '@hourglass/common/helpers';
 
 interface PartProps {
   refreshCodeMirrorsDeps: React.DependencyList;
@@ -36,19 +37,19 @@ const Part: React.FC<PartProps> = (props) => {
   } = part;
   const { rubric } = useContext(ExamViewerContext);
   const pRubric = rubric?.questions[qnum]?.parts[pnum]?.partRubric;
-  const strPoints = points > 1 || points === 0 ? 'points' : 'point';
+  const strPoints = pluralize(points, 'point', 'points');
   let subtitle;
   if (currentGrading?.score !== undefined) {
-    subtitle = `${currentGrading?.score} / ${points} ${strPoints}`;
+    subtitle = `${currentGrading?.score} / ${strPoints}`;
   } else {
-    subtitle = `(${points} ${strPoints})`;
+    subtitle = `(${strPoints})`;
   }
   const contextVal = useMemo(() => ({ references: reference }), [reference]);
   return (
     <PartFilesContext.Provider value={contextVal}>
       <div>
         <h3 id={`question-${qnum}-part-${pnum}`} className="d-flex align-items-baseline">
-          <PartName name={name} pnum={pnum} />
+          <PartName anonymous={anonymous} name={name} pnum={pnum} />
           {anonymous || (
             <span className="ml-auto point-count">
               {subtitle}

app/javascript/components/workflows/proctor/registrations/show/ShowQuestion.tsx
@@ -7,6 +7,7 @@ import { QuestionFilesContext, ExamViewerContext } from '@hourglass/common/conte
 import { QuestionName } from '@student/exams/show/components/ShowQuestion';
 import ShowRubric from '@proctor/registrations/show/ShowRubric';
 import { CurrentGrading } from '@professor/exams/types';
+import { pluralize } from '@hourglass/common/helpers';
 
 interface ShowQuestionProps {
   refreshCodeMirrorsDeps: React.DependencyList;
@@ -32,8 +33,21 @@ const ShowQuestion: React.FC<ShowQuestionProps> = (props) => {
   const qRubric = rubric?.questions[qnum]?.questionRubric;
   const singlePart = parts.length === 1 && !parts[0].name.value;
   const points = parts.reduce((pts, p, _idx) => pts + p.points, 0);
-  const strPoints = points > 1 || points === 0 ? 'points' : 'point';
-  const subtitle = `(${points} ${strPoints})`;
+  const strPoints = pluralize(points, 'point', 'points');
+  let curScore = 0;
+  for (let i = 0; i < parts.length; i += 1) {
+    if (currentGrading[i]?.score !== undefined) {
+      curScore += currentGrading[i].score;
+    } else {
+      curScore = undefined;
+    }
+  }
+  let subtitle;
+  if (curScore !== undefined) {
+    subtitle = `${curScore} / ${strPoints}`;
+  } else {
+    subtitle = `(${strPoints})`;
+  }
   const contextVal = useMemo(() => ({ references: reference }), [reference]);
   return (
     <QuestionFilesContext.Provider value={contextVal}>

app/javascript/components/workflows/student/exams/show/components/Part.tsx
@@ -21,7 +21,12 @@ interface PartProps {
   spyQuestion?: (question: number, pnum?: number) => void;
 }
 
-export const PartName: React.FC<{ pnum: number; name?: HTMLVal }> = ({ pnum, name }) => {
+export const PartName: React.FC<{
+  anonymous: boolean;
+  pnum: number;
+  name?: HTMLVal;
+}> = ({ anonymous, pnum, name }) => {
+  if (anonymous) { return (<></>); }
   if (name === undefined || name.value === '') {
     return <div className="d-inline-block">{`Part ${alphabetIdx(pnum)}`}</div>;
   }
@@ -69,7 +74,7 @@ const Part: React.FC<PartProps> = (props) => {
           />
         )}
         <h3 id={`question-${qnum}-part-${pnum}`} className="d-flex align-items-baseline">
-          <PartName name={name} pnum={pnum} />
+          <PartName anonymous={anonymous} name={name} pnum={pnum} />
           {anonymous || (
             <span className="ml-auto point-count">
               {subtitle}

commit adb03228f09ff34b4d70c996ae7442142cbbd2a0

fix regression introduced by bc69dd0e, which caused a crash when trying to scroll to a question with only one part

app/javascript/components/workflows/student/exams/show/reducers/pagination.ts
@@ -46,14 +46,14 @@ export default (state: PaginationState = {
     case 'SPY_QUESTION':
       return {
         ...state,
-        spy: state.spyCoords.findIndex(sameCoords(action.coords)),
+        spy: findBestPageCoordIdx(state.spyCoords, action.coords),
       };
     case 'PREV_QUESTION': {
       const page = state.page - 1;
       return {
         ...state,
         page,
-        spy: state.spyCoords.findIndex(sameCoords(state.pageCoords[page])),
+        spy: findBestPageCoordIdx(state.spyCoords, state.pageCoords[page]),
       };
     }
     case 'NEXT_QUESTION': {
@@ -61,7 +61,7 @@ export default (state: PaginationState = {
       return {
         ...state,
         page,
-        spy: state.spyCoords.findIndex(sameCoords(state.pageCoords[page])),
+        spy: findBestPageCoordIdx(state.spyCoords, state.pageCoords[page]),
       };
     }
     case 'ACTIVATE_WAYPOINTS':
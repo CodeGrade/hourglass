a88231edd0fb5d00581acb10d8fed242d4e29939
Ben Lerner <blerner@ccs.neu.edu>
2020-10-08 20:27:49 -0400

    automatically notify student that their anomaly has been cleared.  Fixes #188

--- app/graphql/mutations/destroy_anomaly.rb
+++ app/graphql/mutations/destroy_anomaly.rb
@@ -23,13 +23,33 @@ module Mutations
     end
 
     def resolve(anomaly:)
-      updated = anomaly.update(forgiven: true)
-      raise GraphQL::ExecutionError, anomaly.errors.full_messages.to_sentence unless updated
+      Anomaly.transaction do
+        updated = anomaly.update(forgiven: true)
+        raise GraphQL::ExecutionError, "Unable to forgive anomaly: #{anomaly.errors.full_messages.to_sentence}" unless updated
+        msg = Message.new(
+          sender: context[:current_user], 
+          registration: anomaly.registration, 
+          body: "You have been let back into the exam: please refresh this page to resume your exam."
+        )
+        saved = msg.save
+        raise GraphQL::ExecutionError, "Unable to notify student: #{msg.errors.full_messages.to_sentence}" unless saved
 
-      deleted_id = HourglassSchema.id_from_object(anomaly, Types::AnomalyType, context)
-      exam_id = HourglassSchema.id_from_object(anomaly.exam, Types::ExamType, context)
-      HourglassSchema.subscriptions.trigger(:anomaly_was_destroyed, { exam_id: exam_id }, deleted_id)
-      { deleted_id: deleted_id }
+        deleted_id = HourglassSchema.id_from_object(anomaly, Types::AnomalyType, context)
+        exam_id = HourglassSchema.id_from_object(anomaly.exam, Types::ExamType, context)
+        HourglassSchema.subscriptions.trigger(:anomaly_was_destroyed, { exam_id: exam_id }, deleted_id)
+        HourglassSchema.subscriptions.trigger(
+          :message_was_sent,
+          { exam_id: exam_id, },
+          msg,
+        )
+        HourglassSchema.subscriptions.trigger(
+          :message_received,
+          { registration_id: HourglassSchema.id_from_object(msg.registration, Types::RegistrationType, context) },
+          msg,
+        )
+        
+        { deleted_id: deleted_id }
+      end
     end
   end
 end

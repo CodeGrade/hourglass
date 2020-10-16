faa4f403bf8464c310ec63170e7ef78598c72730
Ben Lerner <blerner@ccs.neu.edu>
2020-10-16 13:30:22 -0400

    minor UI nit

--- app/javascript/components/workflows/grading/index.tsx
+++ app/javascript/components/workflows/grading/index.tsx
@@ -1438,46 +1438,48 @@ const GradeOnePartHelp: React.FC<{
           <h1>{registration.exam.name}</h1>
         </Col>
       </Row>
-      <Carousel
-        activeIndex={activeIndex}
-        onSelect={(newIndex, _) => {
-          setActiveIndex(newIndex);
-        }}
-        onSlid={() => refresh()}
-        onSlide={() => refresh()}
-        indicators={false}
-        wrap={false}
-        interval={null}
-        keyboard={false}
-        nextIcon={<Icon I={FaChevronCircleRight} size="3em" />}
-        prevIcon={<Icon I={FaChevronCircleLeft} size="3em" />}
-      >
-        {qpPairs.map((qp, index) => (
-          <Carousel.Item
-            // eslint-disable-next-line react/no-array-index-key
-            key={index}
-            className="mb-4"
-          >
-            <Col className="overflow-auto-y">
-              {(index === indexOfQP) ? (
-                <Grade
-                  registrationKey={registration}
-                  qnum={qp.qnum}
-                  pnum={qp.pnum}
-                  refreshProps={[indexOfQP, refresher]}
-                />
-              ) : (
-                <ShowOnePart
-                  registrationKey={registration}
-                  qnum={qp.qnum}
-                  pnum={qp.pnum}
-                  refreshProps={[indexOfQP, refresher]}
-                />
-              )}
-            </Col>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      <Row>
+        <Carousel
+          activeIndex={activeIndex}
+          onSelect={(newIndex, _) => {
+            setActiveIndex(newIndex);
+          }}
+          onSlid={() => refresh()}
+          onSlide={() => refresh()}
+          indicators={false}
+          wrap={false}
+          interval={null}
+          keyboard={false}
+          nextIcon={<Icon I={FaChevronCircleRight} size="3em" />}
+          prevIcon={<Icon I={FaChevronCircleLeft} size="3em" />}
+        >
+          {qpPairs.map((qp, index) => (
+            <Carousel.Item
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              className="mb-4"
+            >
+              <Col className="overflow-auto-y">
+                {(index === indexOfQP) ? (
+                  <Grade
+                    registrationKey={registration}
+                    qnum={qp.qnum}
+                    pnum={qp.pnum}
+                    refreshProps={[indexOfQP, refresher]}
+                  />
+                ) : (
+                  <ShowOnePart
+                    registrationKey={registration}
+                    qnum={qp.qnum}
+                    pnum={qp.pnum}
+                    refreshProps={[indexOfQP, refresher]}
+                  />
+                )}
+              </Col>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </Row>
     </Container>
   );
 };

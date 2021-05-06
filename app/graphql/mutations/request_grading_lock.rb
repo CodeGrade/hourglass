3a2e791742e97934fdd753e1474933f7e3268660
Ben Lerner <blerner@ccs.neu.edu>
2021-05-06 08:48:39 -0400

    fix request_grading_lock to translate qnum/pnum to question/part

--- app/graphql/mutations/request_grading_lock.rb
+++ app/graphql/mutations/request_grading_lock.rb
@@ -17,7 +17,10 @@ module Mutations
 
     def resolve(registration:, qnum:, pnum:)
       GradingLock.transaction do
-        lock = registration.grading_locks.find_by(registration: registration, qnum: qnum, pnum: pnum)
+        ev = registration.exam_version
+        question = ev.db_questions.find_by(index: qnum)
+        part = question.parts.find_by(index: pnum)
+        lock = registration.grading_locks.find_by(registration: registration, question: question, part: part)
         raise GraphQL::ExecutionError, 'That part is not being graded.' unless lock
 
         if lock.grader

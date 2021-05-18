128086f785e18382e88b06a7f2c771129ee658e7
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-05-17 23:11:41 -0400

    nix: use ruby 2.7.2

--- flake.nix
+++ flake.nix
@@ -1,13 +1,19 @@
 {
   description = "hourglass";
 
-  outputs = { self, nixpkgs }: let
+  inputs.nixpkgs-ruby.url = "github:nixos/nixpkgs/3d7c120";
+
+  outputs = { self, nixpkgs, nixpkgs-ruby }: let
     pkgs = import nixpkgs {
+      system = "x86_64-linux";
+    };
+    rubyPkgs = import nixpkgs-ruby {
       system = "x86_64-linux";
     };
   in {
     devShell.x86_64-linux = import ./shell.nix {
       inherit pkgs;
+      ruby = rubyPkgs.ruby_2_7;
     };
   };
 }

b04fce77b78d01511dd98e0959aa5681554e51b6
Ben Lerner <blerner@ccs.neu.edu>
2021-05-29 10:36:54 -0400

    Text body item mutations working

--- app/javascript/components/workflows/professor/exams/new/editor/body-items/Text.tsx
+++ app/javascript/components/workflows/professor/exams/new/editor/body-items/Text.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext } from 'react';
 import {
   Row, Col, Form,
 } from 'react-bootstrap';
@@ -7,10 +7,13 @@ import {
   useMutation,
 } from 'relay-hooks';
 import Prompted from '@professor/exams/new/editor/body-items/Prompted';
-import { TextInfo, TextState } from '@student/exams/show/types';
+import { DebouncedFormControl } from '@professor/exams/new/editor/components/helpers';
+import { HTMLVal, TextInfo, TextState } from '@student/exams/show/types';
 import { MutationReturn } from '@hourglass/common/helpers';
 import { AlertContext } from '@hourglass/common/alerts';
+
 import { TextCreateMutation } from './__generated__/TextCreateMutation.graphql';
+import { TextChangeMutation } from './__generated__/TextChangeMutation.graphql';
 
 export function useCreateTextMutation(): MutationReturn<TextCreateMutation> {
   const { alert } = useContext(AlertContext);
@@ -41,6 +44,33 @@ export function useCreateTextMutation(): MutationReturn<TextCreateMutation> {
   );
 }
 
+function useChangeTextMutation(): MutationReturn<TextChangeMutation> {
+  const { alert } = useContext(AlertContext);
+  return useMutation<TextChangeMutation>(
+    graphql`
+    mutation TextChangeMutation($input: ChangeTextDetailsInput!) {
+      changeTextDetails(input: $input) {
+        bodyItem {
+          id
+          info
+          answer
+        }
+      }
+    }
+    `,
+    {
+      onError: (err) => {
+        alert({
+          variant: 'danger',
+          title: 'Error changing Text body item',
+          message: err.message,
+          copyButton: true,
+        });
+      },
+    },
+  );
+}
+
 const Text: React.FC<{
   info: TextInfo,
   id: string,
@@ -48,27 +78,52 @@ const Text: React.FC<{
   answer: TextState
 }> = (props) => {
   const {
+    id,
     info,
     answer,
-    disabled = false,
+    disabled: parentDisabled = false,
   } = props;
+  const [mutate, { loading }] = useChangeTextMutation();
+  const updatePrompt = useCallback((newPrompt: HTMLVal) => {
+    mutate({
+      variables: {
+        input: {
+          bodyItemId: id,
+          updatePrompt: true,
+          prompt: newPrompt,
+        },
+      },
+    });
+  }, [id]);
+  const updateAnswer = useCallback((newAnswer: string) => {
+    mutate({
+      variables: {
+        input: {
+          bodyItemId: id,
+          updateAnswer: true,
+          answer: newAnswer,
+        },
+      },
+    });
+  }, [id]);
+  const disabled = parentDisabled || loading;
   return (
     <>
       <Prompted
         value={info.prompt}
         disabled={disabled}
-        onChange={console.log}
+        onChange={updatePrompt}
       />
       <Form.Group as={Row}>
         <Form.Label column sm={2}>Correct answer</Form.Label>
         <Col sm={10}>
-          <Form.Control
+          <DebouncedFormControl
             as="textarea"
             rows={3}
             disabled={disabled}
             placeholder="Sketch the intended answer here."
-            value={answer ?? ''}
-            onChange={console.log}
+            defaultValue={answer ?? ''}
+            onChange={updateAnswer}
           />
         </Col>
       </Form.Group>

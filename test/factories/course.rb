aa63e20614380d5b62e0dc2f7798bbbe359367ac
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-09-14 10:05:54 -0400

    fix term factory

--- test/factories/course.rb
+++ test/factories/course.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :course do
     sequence(:title, 101) { |n| "Computing #{n}" }
+    term
     last_sync { '2020-05-22 14:03:53' }
     active { true }
     sequence(:bottlenose_id)

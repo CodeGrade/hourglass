6f7aed496fcfc52c7c09363c692cc6d3dc1f869a
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-09-23 10:13:18 -0400

    remove another qnum, pnum, bnum from one.rb

--- app/models/rubrics/one.rb
+++ app/models/rubrics/one.rb
@@ -11,13 +11,11 @@ class One < Rubric
   end
 
   def compute_grade_for(reg, comments, checks, qpb)
-    qnum, pnum, bnum = qpb
     if rubric_preset
       preset_ids = rubric_preset.preset_comment_ids
-      if comments.dig(qnum, pnum, bnum)&.slice(*preset_ids)&.count.to_i > 0
+      if comments.dig(*qpb)&.slice(*preset_ids)&.count.to_i > 0
         rubric_preset.compute_grade_for(reg, out_of, comments, checks, qpb)
       else
-        puts "HERE1"
         points
       end
     else
@@ -26,7 +24,6 @@ class One < Rubric
         puts "Found used_section #{used_section.type} #{used_section.id}"
         used_section.compute_grade_for(reg, comments, checks, qpb)
       else
-        puts "HERE2"
         points
       end
     end

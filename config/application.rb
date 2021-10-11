f89d9ce0ec8c63138db61baa387b3d832dc3bb8e
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-10-11 16:37:34 -0400

    rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -3,13 +3,18 @@
 require_relative 'boot'
 
 require 'rails'
+# Pick the frameworks you want:
 require 'active_model/railtie'
 require 'active_job/railtie'
-require 'action_cable/engine'
 require 'active_record/railtie'
+# require 'active_storage/engine'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
+# require 'action_mailbox/engine'
+# require 'action_text/engine'
 require 'action_view/railtie'
+require 'action_cable/engine'
+# require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -18,6 +23,7 @@ Bundler.require(*Rails.groups)
 
 module Hourglass
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
     config.before_configuration do
@@ -32,11 +38,15 @@ module Hourglass
     # Add subdirectories of models
     config.autoload_paths += Dir[Rails.root.join("app", "models", "{*/}")]
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
     config.autoload_paths << Rails.root.join('lib')
+
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
@@ -9,6 +8,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.cache_classes = false
+  config.action_view.cache_template_loading = true
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
@@ -42,6 +42,15 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation = :raise
+
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
+
   # Raises error for missing translations.
-  # config.action_view.raise_on_missing_translations = true
+  # config.i18n.raise_on_missing_translations = true
+
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
 end

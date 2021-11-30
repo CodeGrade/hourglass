cb06d9ce4e63dff65f81aa10df5eeb2cb069d5ff
Ben Lerner <blerner@ccs.neu.edu>
2021-11-30 15:48:10 -0500

    also fix the association loader, to load things correctly...

--- app/graphql/types/exam_type.rb
+++ app/graphql/types/exam_type.rb
@@ -44,7 +44,7 @@ module Types
       guard Guards::PROCTORS_AND_PROFESSORS
     end
     def anomalies
-      AssociationLoader.for(Exam, :anomalies, merge: -> { includes(registration: { accommodation: { exam_version: :exam } }).unforgiven }).load(object)
+      AssociationLoader.for(Exam, :anomalies, merge: -> { includes(registration: [ :accommodation, { exam_version: :exam } ]).unforgiven }).load(object)
     end
 
     field :messages, Types::MessageType.connection_type, null: false do

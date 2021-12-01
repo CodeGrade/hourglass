6adc02029ca364046d3f76644dc934f8d1234b16
Ben Lerner <blerner@ccs.neu.edu>
2021-12-01 11:02:06 -0500

    add validation to ensure references aren't owned by both questions and parts, and make it more convenient to retrieve the contents of a reference from its examversion

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -7,6 +7,7 @@ class Reference < ApplicationRecord
   belongs_to :part, optional: true, inverse_of: :references
 
   validate :valid_path
+  validate :not_both_question_and_part
 
   def valid_path
     return if exam_version.has_file_or_dir_path? path
@@ -20,6 +21,24 @@ class Reference < ApplicationRecord
     location = "question #{part.question.index}, part #{part.index}" unless part.nil?
     location = 'exam version' if question.nil? && part.nil?
     errors.add(:path, "does not exist in the upload: '#{path}' (invalid reference for #{location})")
+  end
+
+  def not_both_question_and_part
+    return unless question.present? && part.present?
+
+    errors.add(:base, 'Reference cannot belong to both a question and a part.')
+  end
+
+  def contents
+    all_files = exam_version.files
+    path_components = path.split('/')
+    path_components.each do |pc|
+      break if all_files.nil?
+      
+      all_files = all_files['nodes'] if all_files.is_a? Hash
+      all_files = all_files.find { |f| f['path'] == pc }
+    end
+    all_files
   end
 
   def self.inheritance_column

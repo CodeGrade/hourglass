a0b576b8dc06041555f482aa3cc6eed4b4c19f74
Kyle Sferrazza <git@kylesferrazza.com>
2022-03-01 11:55:41 -0500

    relay-hooks: 404 page

--- app/packs/components/workflows/FourOhFour.tsx
+++ app/packs/components/workflows/FourOhFour.tsx
@@ -1,63 +1,98 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   Alert,
   Table,
 } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import DocumentTitle from '@hourglass/common/documentTitle';
-import { useQuery, graphql } from 'relay-hooks';
+import { useLazyLoadQuery, graphql } from 'react-relay';
 import { DateTime } from 'luxon';
 import { FourOhFourQuery } from '@hourglass/workflows/__generated__/FourOhFourQuery.graphql';
+import ErrorBoundary from '@hourglass/common/boundary';
 
 const FourOhFour: React.FC = () => {
   const { pathname } = useLocation();
-  const res = useQuery<FourOhFourQuery>(
-    graphql`
-    query FourOhFourQuery {
-      me {
-        displayName
-      }
-    }
-  `,
-  );
   return (
-    <DocumentTitle title="Not found">
-      <div className="wrapper">
-        <div className="justify-content-center flex-grow-1 align-items-center d-flex flex-column">
-          <div className="d-flex flex-column align-items-center">
-            <img src="/find_x.jpg" alt="Find 'x'" />
-            <h2>
-              Unfortunately, finding
-              <code className="mx-2">{pathname}</code>
-              isn&rsquo;t as easy as finding
-              <code className="ml-2">x</code>
-              ...
-            </h2>
-            <p>Please contact a professor, and provide them with the following information:</p>
-            <Alert variant="warning">
-              <Table size="sm" className="mb-0" borderless>
-                <tbody>
+    <ErrorBoundary>
+      <Suspense
+        fallback={(
+          <FourOhFourDisp
+            pathname={pathname}
+          />
+        )}
+      >
+        <FourOhFourQueryComp
+          pathname={pathname}
+        />
+      </Suspense>
+    </ErrorBoundary>
+  );
+};
+
+const FourOhFourDisp: React.FC<{
+  pathname: string;
+  displayName?: string;
+}> = ({
+  pathname,
+  displayName,
+}) => (
+  <DocumentTitle title="Not found">
+    <div className="wrapper">
+      <div className="justify-content-center flex-grow-1 align-items-center d-flex flex-column">
+        <div className="d-flex flex-column align-items-center">
+          <img src="/find_x.jpg" alt="Find 'x'" />
+          <h2>
+            Unfortunately, finding
+            <code className="mx-2">{pathname}</code>
+            isn&rsquo;t as easy as finding
+            <code className="ml-2">x</code>
+            ...
+          </h2>
+          <p>Please contact a professor, and provide them with the following information:</p>
+          <Alert variant="warning">
+            <Table size="sm" className="mb-0" borderless>
+              <tbody>
+                <tr>
+                  <td>Location:</td>
+                  <td><code>{window.location.toString()}</code></td>
+                </tr>
+                <tr>
+                  <td>Time:</td>
+                  <td><code>{DateTime.local().toISO()}</code></td>
+                </tr>
+                {displayName && (
                   <tr>
-                    <td>Location:</td>
-                    <td><code>{window.location.toString()}</code></td>
+                    <td>Username:</td>
+                    <td><code>{displayName}</code></td>
                   </tr>
-                  <tr>
-                    <td>Time:</td>
-                    <td><code>{DateTime.local().toISO()}</code></td>
-                  </tr>
-                  {res.data && (
-                    <tr>
-                      <td>Username:</td>
-                      <td><code>{res.data.me.displayName}</code></td>
-                    </tr>
-                  )}
-                </tbody>
-              </Table>
-            </Alert>
-          </div>
+                )}
+              </tbody>
+            </Table>
+          </Alert>
         </div>
       </div>
-    </DocumentTitle>
+    </div>
+  </DocumentTitle>
+);
+
+const FourOhFourQueryComp: React.FC<{
+  pathname: string;
+}> = ({ pathname }) => {
+  const res = useLazyLoadQuery<FourOhFourQuery>(
+    graphql`
+      query FourOhFourQuery {
+        me {
+          displayName
+        }
+      }
+    `,
+    {},
+  );
+  return (
+    <FourOhFourDisp
+      pathname={pathname}
+      displayName={res.me.displayName}
+    />
   );
 };
 

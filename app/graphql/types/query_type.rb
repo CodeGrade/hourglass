0efd997783f89619814252c9c4755257f2b6822d
Kyle Sferrazza <git@kylesferrazza.com>
2023-05-07 13:31:34 -0400

    fix deprecated graphql relay fields

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,8 +4,8 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-    add_field(GraphQL::Types::Relay::NodeField)
-    add_field(GraphQL::Types::Relay::NodesField)
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
 
     field :impersonating, Boolean, null: false
     def impersonating

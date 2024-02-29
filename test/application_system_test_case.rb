19580658da4c94c081e5e738b3577589bd450700
Ben Lerner <blerner@ccs.neu.edu>
2024-02-29 14:38:58 -0500

    generalize the driver being used for system tests, so that it can be easily swapped out to visibly render tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,5 +4,10 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Devise::Test::IntegrationHelpers
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  DRIVER = if ENV["DRIVER"]
+    ENV["DRIVER"].to_sym
+  else
+    :headless_chrome
+  end
+  driven_by :selenium, using: DRIVER, screen_size: [1400, 1400]
 end
